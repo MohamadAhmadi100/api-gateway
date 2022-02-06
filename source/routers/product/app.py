546e705226140a8a238c5b4b42e65d3e5d03eaef
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-02-06 14:24:04 +0330

    added new endpointes

--- source/routers/product/app.py
+++ source/routers/product/app.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, HTTPException, Response, responses
+from fastapi import FastAPI, HTTPException, Response, responses, Path, Body, Query
 from starlette.exceptions import HTTPException as starletteHTTPException
 
+from source.routers.product.validators import CreateParent
 from source.config import settings
 from source.message_broker.rabbit_server import RabbitRPC
+
 # from source.routers.quantity.validators.quantity import Quantity
+from source.routers.product.validators.product import CreateChild, AddAtributes
 
 TAGS = [
     {
@@ -54,6 +57,261 @@ def get_parent_configs(system_code: str, response: Response) -> dict:
                         detail={"error": product_result.get("error", "Something went wrong")})
 
 
+@app.get("/product/parent/", status_code=200)
+def create_parent_schema():
+    return CreateParent.schema().get("properties")
+
+
+@app.post("/product/parent/", status_code=201)
+def create_parent(
+        item: CreateParent, response: Response
+) -> dict:
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "create_parent",
+                "body": dict(item)
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@app.get("/product/{system_code}/items", status_code=200)
+def suggest_product(response: Response, system_code: str = Path(..., min_length=11, max_length=11)) -> dict:
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "suggest_product",
+                "body": {
+                    "system_code": system_code
+                }
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@app.get("/product/child/", status_code=200)
+def create_child_schema():
+    return CreateChild.schema().get("properties")
+
+
+@app.post("/product/child/", status_code=201)
+def create_child(
+        item: CreateChild, response: Response
+) -> dict:
+    """
+    Create a product for sale in main collection in database.
+    attributes will be validated before insert.
+    """
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "create_child",
+                "body": dict(item)
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@app.get("/product/attributes/", status_code=200)
+def add_attributes_schema():
+    return AddAtributes.schema().get("properties")
+
+
+@app.post("/product/attributes/", status_code=201)
+def add_attributes(response: Response,
+                   item: AddAtributes = Body(..., example={
+                       "system_code": "100104021006",
+                       "attributes": {
+                           "image": "/src/default.jpg",
+                           "year": 2020
+                       }
+                   })
+                   ) -> dict:
+    """
+    Create a product for sale in main collection in database.
+    attributes will be validated before insert.
+    """
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "add_attributes",
+                "body": dict(item)
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@app.get("/product/{system_code}/{lang}", status_code=200)
+def get_product_by_system_code(
+        response: Response,
+        system_code: str = Path(..., min_length=11, max_length=11),
+        lang: str = Path("fa_ir", min_length=2, max_length=127)
+) -> dict:
+    """
+    Get a product by system_code in main collection in database.
+    """
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "get_product_by_system_code",
+                "body": {
+                    "system_code": system_code,
+                    "lang": lang
+                }
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@app.delete("/product/{system_code}", status_code=200)
+def delete_product(
+        response: Response,
+        system_code: str = Path(..., min_length=3, max_length=255)
+) -> dict:
+    """
+    Delete a product by name in main collection in database.
+    """
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "delete_product",
+                "body": {
+                    "system_code": system_code
+                }
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@app.get("/product/update_attribute_collection/", status_code=200)
+def update_attribute_collection(response: Response) -> dict:
+    """
+    Update the attribute collection in database.
+    """
+    # TODO: Later, the attributes below should come from API GW
+    attributes = [
+        {
+            "required": True,
+            "use_in_filter": False,
+            "use_for_sort": False,
+            "default_value": None,
+            "values": None,
+            "set_to_nodes": False,
+            "name": "year",
+            "label": "سال",
+            "input_type": "Number",
+            "parent": "100104021006"
+        },
+        {
+            "required": False,
+            "use_in_filter": False,
+            "use_for_sort": False,
+            "default_value": "/src/default.png",
+            "values": None,
+            "set_to_nodes": True,
+            "name": "image",
+            "label": "عکس",
+            "input_type": "Media Image",
+            "parent": "1001"
+        }
+    ]
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "update_attribute_collection",
+                "body": {
+                    "attributes": attributes
+                }
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@app.get("/categories/{system_code}/")
+def get_all_categories(
+                       response: Response,
+                       system_code: str = Path(00, min_length=2, max_length=6),
+                       page: int = Query(1, ge=1, le=1000),
+                       per_page: int = Query(15, ge=1, le=1000)):
+    """
+    """
+    rpc.response_len_setter(response_len=1)
+    product_result = rpc.publish(
+        message={
+            "product": {
+                "action": "get_all_categories",
+                "body": {
+                    "system_code": system_code,
+                    "page": page,
+                    "per_page": per_page
+                }
+            }
+        },
+        headers={'product': True}
+    )
+    product_result = product_result.get("product", {})
+    if product_result.get("success"):
+        response.status_code = product_result.get("status_code", 200)
+        return product_result.get("message")
+    raise HTTPException(status_code=product_result.get("status_code", 500),
+                        detail={"error": product_result.get("error", "Something went wrong")})
 
 #
 # @app.post("/api/v1/product/quantity/", tags=["Quantity"])
---
+++ source/routers/product/validators/product.py
@@ -0,0 +1,91 @@
+from typing import Optional
+
+from fastapi import HTTPException
+from pydantic import BaseModel, validator, Field
+
+
+class CreateParent(BaseModel):
+    system_code: str = Field(
+        ..., title="شناسه محصول", maxLength=11, minLength=11, placeholder="10010402101", isRequired=True
+    )
+    name: Optional[str] = Field(
+        None, title="نام", minLength=3, maxLength=256, placeholder="ردمی ۹ سی", isRequired=False
+    )
+    visible_in_site: bool
+
+    @validator('system_code')
+    def system_code_validator(cls, value):
+        if not isinstance(value, str):
+            raise HTTPException(status_code=417, detail={
+                "error": "system code must be a string",
+                "label": "کد سیستمی باید از نوع رشته باشد"
+            })
+        elif len(value) != 11:
+            raise HTTPException(status_code=417, detail={
+                "error": "system_code must be 11 characters",
+                "label": "طول شناسه محصول باید ۱۱ کاراکتر باشد"
+            })
+        return value
+
+    @validator('name')
+    def name_validator(cls, value):
+        if not isinstance(value, str):
+            raise HTTPException(status_code=417, detail={
+                "error": 'name must be a string',
+                "label": "اسم باید از نوع رشته باشد"
+            })
+        elif len(value) < 3 or len(value) > 256:
+            raise HTTPException(status_code=417, detail={
+                "error": "name must be between 3 and 256 characters",
+                "label": "طول اسم باید میان ۳ تا ۲۵۶ کاراکتر باشد"
+            })
+        return value
+
+    @validator('visible_in_site')
+    def visible_in_site_validator(cls, value):
+        if not isinstance(value, bool):
+            raise HTTPException(status_code=417, detail={
+                "error": 'visible_in_site must be a boolean',
+                "label": "نمایش در سایت باید از نوع بولی باشد"
+            })
+        return value
+
+
+class CreateChild(BaseModel):
+    parent_system_code: str = Field(
+        ..., title="شناسه اصلی محصول", maxLength=11, minLength=11, placeholder="10010402101", isRequired=True
+    )
+    system_code: str = Field(
+        ..., title="شناسه محصول", maxLength=12, minLength=12, placeholder="100104021006", isRequired=True
+    )
+    visible_in_site: bool
+
+    @validator('system_code')
+    def system_code_validator(cls, value):
+        if not isinstance(value, str):
+            raise HTTPException(status_code=417, detail={
+                "error": "system codes must be a string",
+                "label": "شناسه های محصولات باید از نوع رشته باشد"
+            })
+        elif len(value) != 12:
+            raise HTTPException(status_code=417, detail={
+                "error": "system_code must be 12 characters",
+                "label": "طول شناسه محصول باید ۱۲ کاراکتر باشد"
+            })
+        return value
+
+    @validator('visible_in_site')
+    def visible_in_site_validator(cls, value):
+        if not isinstance(value, bool):
+            raise HTTPException(status_code=417, detail={
+                "error": 'visible_in_site must be a boolean',
+                "label": "نمایش در سایت باید از نوع بولی باشد"
+            })
+        return value
+
+
+class AddAtributes(BaseModel):
+    system_code: str = Field(
+        ..., title="شناسه محصول", maxLength=12, minLength=12, placeholder="100104021006", isRequired=True
+    )
+    attributes: dict
