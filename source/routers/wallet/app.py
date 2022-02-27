5a8fb6142abf56c976eb350c8d0c1380cf02fc22
zhra-jm <zahrajam1999@gmail.com>
2022-02-27 14:07:51 +0330

    get transaction added

--- source/routers/wallet/app.py
+++ source/routers/wallet/app.py
@@ -3,6 +3,7 @@ from starlette.exceptions import HTTPException as starletteHTTPException
 from source.config import settings
 from source.message_broker.rabbit_server import RabbitRPC
 from source.routers.wallet.validators.wallet import Wallet
+from source.routers.wallet.validators.transaction import Transaction
 from source.routers.wallet.validators.update_wallet import UpdateData
 
 TAGS = [
@@ -92,6 +93,27 @@ def update_wallet(data: UpdateData, response: Response):
         headers={'wallet': True}
     ).get("wallet", {})
 
+    if wallet_response.get("success"):
+        response.status_code = wallet_response.get("status_code", 200)
+        return wallet_response
+    raise HTTPException(status_code=wallet_response.get("status_code", 500),
+                        detail={"error": wallet_response.get("error", "Wallet service Internal error")})
+
+
+@app.post("/transactions_details", tags=["transactions"])
+def gte_transactions(data: Transaction, response: Response):
+    rpc.response_len_setter(response_len=1)
+    wallet_response = rpc.publish(
+        message={
+            "wallet": {
+                "action": "get_transaction",
+                "body": {
+                    "data": dict(data)
+                }
+            }
+        },
+        headers={'wallet': True}
+    ).get("wallet", {})
     if wallet_response.get("success"):
         response.status_code = wallet_response.get("status_code", 200)
         return wallet_response
