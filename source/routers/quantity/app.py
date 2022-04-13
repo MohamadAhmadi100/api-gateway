4599f30b9db09ed190ba73153ca5e5849a52f957
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-04-13 14:13:22 +0430

    added price to quantity object

--- source/routers/quantity/app.py
+++ source/routers/quantity/app.py
@@ -39,12 +39,12 @@ rpc.consume()
 
 @app.get("/product/quantity_page/{systemCode}/", tags=["Quantity"])
 def get_product_quantity_page(response: Response,
-                           system_code: str = Path(..., alias="systemCode", max_length=12, min_length=12),
-                           lang: Optional[str] = "fa_ir"):
+                              system_code: str = Path(..., alias="systemCode", max_length=12, min_length=12),
+                              lang: Optional[str] = "fa_ir"):
     """
     get product price page
     """
-    rpc.response_len_setter(response_len=2)
+    rpc.response_len_setter(response_len=3)
     price_page = rpc.publish(
         message={
             "product": {
@@ -53,22 +53,57 @@ def get_product_quantity_page(response: Response,
                     "system_code": system_code,
                     "lang": lang
                 }
-            }, "quantity": {
+            },
+            "quantity": {
                 "action": "get_quantity_child",
                 "body": {
                     "system_code": system_code
                 }
-            }}
+            },
+            "pricing": {
+                "action": "get_price_child",
+                "body": {
+                    "system_code": system_code
+                }
+            }
+        }
         ,
-        headers={"product": True, "quantity": True}
+        headers={"product": True, "quantity": True, "pricing": True}
     )
     product_result = price_page.get("product", {})
     quantity_result = price_page.get("quantity", {})
+    pricing_result = price_page.get("pricing", {})
     if product_result.get("success"):
         response.status_code = product_result.get("status_code", 200)
+        if quantity_result.get("success"):
+            quantity = quantity_result.get("message", {}).get("products", {}).get(system_code, None)
+            quantity['price'] = pricing_result.get("message", {}).get("products", {}).get(system_code, {}).get(
+                "regular", None)
+            quantity['special'] = pricing_result.get("message", {}).get("products", {}).get(system_code, {}).get(
+                "special", None)
+
+            for key, value in quantity["customer_types"].items():
+                quantity["customer_types"][key]['price'] = pricing_result.get("message", {}).get("products", {}).get(
+                    system_code, {}).get("customer_type", {}).get(key, {}).get("regular", None)
+                quantity["customer_types"][key]['special'] = pricing_result.get("message", {}).get("products", {}).get(
+                    system_code, {}).get("customer_type", {}).get(key, {}).get("special", None)
+                if value.get("storages"):
+                    for key2, value2 in value["storages"].items():
+                        value["storages"][key2]['price'] = pricing_result.get("message", {}).get(
+                            "products", {}).get(system_code, {}).get("customer_type", {}).get(key, {}).get(
+                            "storages", {}).get(key2, {}).get("regular", None)
+                        value["storages"][key2]['special'] = pricing_result.get("message", {}).get(
+                            "products", {}).get(system_code, {}).get("customer_type", {}).get(key, {}).get(
+                            "storages", {}).get(key2, {}).get("special", None)
+
+            return convert_case({
+                "product": product_result.get("message"),
+                "quantity": quantity
+            }, "camel")
+
         return convert_case({
             "product": product_result.get("message"),
-            "quantity": quantity_result.get("message", {}).get("products", {}).get(system_code, None)
+            "quantity": None
         }, 'camel')
     raise HTTPException(status_code=product_result.get("status_code", 500),
                         detail=product_result.get("error", "something went wrong"))
