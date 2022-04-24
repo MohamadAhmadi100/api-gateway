4b954b9c98c33746c33363f5fbfdb5353b93dabe
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-04-24 11:35:59 +0430

    delete pricing keys

--- source/routers/pricing/validators/pricing_validator.py
+++ source/routers/pricing/validators/pricing_validator.py
@@ -9,10 +9,6 @@ from source.routers.pricing.validators.customer_validator import CustomerTypeMod
 class Price(BaseModel):
     parent_system_code: str = Field(..., alias='parentSystemCode')
     system_code: str = Field(..., alias='systemCode')
-    regular: int
-    special: Optional[int] = None
-    special_from_date: str = Field(..., alias='specialFromDate')
-    special_to_date: str = Field(..., alias='specialToDate')
     customer_type: CustomerTypeModel = Field({}, alias='customerType')
 
     @validator("parent_system_code")
@@ -35,58 +31,15 @@ class Price(BaseModel):
                                     detail={"error": "system_code must be between 1 and 100000000000000"})
             return value
 
-    @validator("regular")
-    def regular_validator(cls, value):
-        if not isinstance(value, int):
-            raise HTTPException(status_code=422, detail={"error": "regular must be int"})
-        elif 100000000000000 < value or value < 1:
-            raise HTTPException(status_code=422, detail={"error": "regular must be between 1 and 100000000000000"})
-        return value
-
-    @validator("special")
-    def special_validator(cls, value):
-        if value:
-            if not isinstance(value, int):
-                raise HTTPException(status_code=422, detail={"error": "special must be int"})
-            elif 100000000000000 < value or value < 1:
-                raise HTTPException(status_code=422, detail={"error": "special must be between 1 and 100000000000000"})
-        return value
-
-    @validator("special_from_date")
-    def special_from_date_validator(cls, value):
-        if not isinstance(value, str):
-            raise HTTPException(status_code=422, detail={"error": "special_from_date must be str"})
-        elif 100000000000000 < len(value) or len(value) < 1:
-            raise HTTPException(status_code=422,
-                                detail={"error": "special_from_date must be between 1 and 100000000000000"})
-        return value
-
-    @validator("special_to_date")
-    def special_to_date_validator(cls, value):
-        if not isinstance(value, str):
-            raise HTTPException(status_code=422, detail={"error": "special_to_date must be str"})
-        elif 100000000000000 < len(value) or len(value) < 1:
-            raise HTTPException(status_code=422,
-                                detail={"error": "special_to_date must be between 1 and 100000000000000"})
-        return value
-
     class Config:
         schema_extra = {
             "example": {
                 "parentSystemCode": "10010100201",
                 "systemCode": "100101002001",
-                "regular": 60000000,
-                "special": 50000000,
-                "specialFromDate": "2020-01-01",
-                "specialToDate": "2020-01-01",
                 "customerType":
                     {
                         "B2B": {
                             'type': 'B2B',
-                            "regular": 60000000,
-                            "special": 50000000,
-                            "specialFromDate": "2020-01-01",
-                            "specialToDate": "2020-01-01",
                             "storages": [
                                 {
                                     "storageId": "0",
@@ -103,22 +56,6 @@ class Price(BaseModel):
                                     "specialToDate": "2020-01-01"
                                 }
                             ]
-                        },
-                        "B2C": {
-                            'type': 'B2C',
-                            "regular": 60000000,
-                            "special": 50000000,
-                            "specialFromDate": "2020-01-01",
-                            "specialToDate": "2020-01-01",
-                            "storages": []
-                        },
-                        "B2G": {
-                            'type': 'B2G',
-                            "regular": 60000000,
-                            "special": 50000000,
-                            "specialFromDate": "2020-01-01",
-                            "specialToDate": "2020-01-01",
-                            "storages": []
                         }
                     }
             }
@@ -128,10 +65,6 @@ class Price(BaseModel):
         return {
             'parent_system_code': self.parent_system_code,
             "system_code": self.system_code,
-            'regular': self.regular,
-            'special': self.special,
-            'special_from_date': self.special_from_date,
-            'special_to_date': self.special_to_date,
             'customer_type': self.customer_type.dict()
         }
 
