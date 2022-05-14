efecb69aaa8fc3c6e6493bbd7bcedf8b610898d4
SalarRezaee <salar_rezaei@aol.com>
2022-05-14 17:27:49 +0430

    add shipment to cart in order router and get cart

--- source/routers/order/app.py
+++ source/routers/order/app.py
@@ -6,6 +6,9 @@ from source.message_broker.rabbit_server import RabbitRPC
 from source.routers.customer.module.auth import AuthHandler
 from source.routers.order.validators.order import check_out
 from source.routers.order.helpers.shipment_requests import build_object
+from source.routers.shipment.validators.shipment_per_stock import PerStock
+from source.routers.cart.helpers.get_cart_helper import get_cart
+
 TAGS = [
     {
         "name": "Order",
@@ -85,3 +88,49 @@ def test(response: Response, auth_header=Depends(auth_handler.check_current_user
             return cart_response
         raise HTTPException(status_code=cart_response.get("status_code", 500),
                             detail={"error": cart_response.get("error", "Cart service Internal error")})
+
+
+
+@app.put("/add", tags=["add shipment to cart and get new cart"])
+def shipment_per_stock(
+        response: Response,
+        data: PerStock,
+        auth_header=Depends(auth_handler.check_current_user_tokens)
+):
+    user, token_dict = auth_header
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        rpc.response_len_setter(response_len=1)
+        shipment_response = rpc.publish(
+            message={
+                "shipment": {
+                    "action": "get_selected_method",
+                    "body": {
+                        "data": data.json()
+                    }
+                }
+            },
+            headers={'shipment': True}
+        ).get("shipment", {})
+        if shipment_response.get("success"):
+            rpc.response_len_setter(response_len=1)
+            cart_response = rpc.publish(
+                message={
+                    "cart": {
+                        "action": "add_shipment_to_cart",
+                        "body": {
+                            "shipment": {
+                                "shipment_details": shipment_response.get("message"),
+                                "user_id": user.get("user_id")
+                            }
+                        }
+                    }
+                },
+                headers={'cart': True}
+            ).get("cart", {})
+
+            if cart_response.get("success"):
+                cart = get_cart(user)
+                response.status_code = cart.get("status_code", 200)
+                return cart
+            raise HTTPException(status_code=cart_response.get("status_code", 500),
+                                detail={"error": cart_response.get("error", "Cart service Internal error")})
