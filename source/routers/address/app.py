c42b107c99656dc601abc5bf02fd51334b3529b1
SalarRezaee <salar_rezaei@aol.com>
2022-05-24 17:36:45 +0430

    add get default address api

--- source/routers/address/app.py
+++ source/routers/address/app.py
@@ -171,3 +171,28 @@ def delete_address(addressId: int, response: Response, auth_header=Depends(auth_
             return address_response
         raise HTTPException(status_code=address_response.get("status_code", 500),
                             detail={"error": address_response.get("error", "Address service Internal error")})
+
+
+
+@app.get("/default_address", tags=["Address"])
+def customer_addresses(response: Response, auth_header=Depends(auth_handler.check_current_user_tokens)):
+    user, token_dict = auth_header
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        rpc.response_len_setter(response_len=1)
+        address_response = rpc.publish(
+            message={
+                "address": {
+                    "action": "get_default_address",
+                    "body": {
+                        "customerId": user.get("user_id")
+                    }
+                }
+            },
+            headers={'address': True}
+        ).get("address", {})
+
+        if address_response.get("success"):
+            response.status_code = address_response.get("status_code", 200)
+            return address_response
+        raise HTTPException(status_code=address_response.get("status_code", 500),
+                            detail={"error": address_response.get("error", "Address service Internal error")})
