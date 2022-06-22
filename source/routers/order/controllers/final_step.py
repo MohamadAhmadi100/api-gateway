3960eea8f0781b399fe0accc1b18bb19eb5aff48
Mohsen Yousefi <mohsen.u3fi@hotmail.com>
2022-06-22 11:06:00 +0430

    final flag commented

--- source/routers/order/controllers/final_step.py
+++ source/routers/order/controllers/final_step.py
@@ -27,75 +27,75 @@ def final_order(
 ) -> dict:
     cart = get_cart(response=response, auth_header=auth_header)
     # check if customer have a processing order
-    if cart.get("finalFlag"):
-        return {"success": False, "message": [{"message": "شما یک سفارش در حال پردازش دارید."}]}
-    else:
+    # if cart.get("finalFlag"):
+    #     return {"success": False, "message": [{"message": "شما یک سفارش در حال پردازش دارید."}]}
+    # else:
         # check if customer select all the shipment methods per stock
-        check_shipment_result = check_shipment_per_stock(cart)
-        if len(cart['shipment']) != len(check_shipment_result):
-            response.status_code = 202
-            return {"success": False, "message": "!روش ارسال برای همه انبار ها را انتخاب کنید"}
-        elif len(cart['payment']) < 1:
-            response.status_code = 202
-            return {"success": False, "message": "!روش پرداخت را انتخاب کنید"}
+    check_shipment_result = check_shipment_per_stock(cart)
+    if len(cart['shipment']) != len(check_shipment_result):
+        response.status_code = 202
+        return {"success": False, "message": "!روش ارسال برای همه انبار ها را انتخاب کنید"}
+    elif len(cart['payment']) < 1:
+        response.status_code = 202
+        return {"success": False, "message": "!روش پرداخت را انتخاب کنید"}
 
-        # check quantity
-        check_out = check_price_qty(auth_header, cart, response)
-        if check_out.get("success"):
-            # create order if all data completed
-            customer = get_profile_info(auth_header[0])
-            place_order_result = place_order(auth_header, cart, customer)
-            if place_order_result.get("success"):
-                result_reserve = reserve_order_items(place_order_result.get("order_object"))
-                if result_reserve.get("success"):
-                    # consume wallet
-                    if cart['payment'].get("walletAmount") is not None:
-                        wallet_final_consume(place_order_result, cart, auth_header, response)
+    # check quantity
+    check_out = check_price_qty(auth_header, cart, response)
+    if check_out.get("success"):
+        # create order if all data completed
+        customer = get_profile_info(auth_header[0])
+        place_order_result = place_order(auth_header, cart, customer)
+        if place_order_result.get("success"):
+            result_reserve = reserve_order_items(place_order_result.get("order_object"))
+            if result_reserve.get("success"):
+                # consume wallet
+                if cart['payment'].get("walletAmount") is not None:
+                    wallet_final_consume(place_order_result, cart, auth_header, response)
 
-                    if place_order_result.get("Type") == "pending_payment":
-                        add_final_flag_to_cart(auth_header)
-                        send_data = SendData(
-                            amount=int(place_order_result.get("bank_request").get("amount")) * 10,
-                            customerId=int(place_order_result.get("bank_request").get("customerId")),
-                            serviceName=place_order_result.get("bank_request").get("serviceName"),
-                            serviceId=int(place_order_result.get("bank_request").get('serviceId')),
-                        )
-                        send_data = convert_case(send_data, "snake")
-                        payment_result = get_url(
-                            data=send_data,
-                            response=response
-                        )
-                        if payment_result.get("success"):
-                            response.status_code = payment_result.get("status_code")
-                            return {"success": True, "Type": "pending_payment",
-                                    "paymentResult": payment_result.get("message")}
-                        else:
-                            return {"success": False, "paymentResult": payment_result.get("error")}
+                if place_order_result.get("Type") == "pending_payment":
+                    add_final_flag_to_cart(auth_header)
+                    send_data = SendData(
+                        amount=int(place_order_result.get("bank_request").get("amount")) * 10,
+                        customerId=int(place_order_result.get("bank_request").get("customerId")),
+                        serviceName=place_order_result.get("bank_request").get("serviceName"),
+                        serviceId=int(place_order_result.get("bank_request").get('serviceId')),
+                    )
+                    send_data = convert_case(send_data, "snake")
+                    payment_result = get_url(
+                        data=send_data,
+                        response=response
+                    )
+                    if payment_result.get("success"):
+                        response.status_code = payment_result.get("status_code")
+                        return {"success": True, "Type": "pending_payment",
+                                "paymentResult": payment_result.get("message")}
                     else:
-
-                        rpc.response_len_setter(response_len=1)
-                        rpc.publish(
-                            message={
-                                "cart": {
-                                    "action": "delete_cart",
-                                    "body": {
-                                        "user_id": auth_header[0].get("user_id")
-                                    }
-                                }
-                            },
-                            headers={'cart': True}
-                        )
-                        response.status_code = place_order_result.get("status_code")
-                        return place_order_result
+                        return {"success": False, "paymentResult": payment_result.get("error")}
                 else:
-                    delete_order_reserving_fail(place_order_result.get("order_object"))
-                    check_out = check_price_qty(auth_header, cart, response)
-                    response.status_code = 202
-                    return {"success": False, "message": check_out.get("message")}
-            else:
 
-                response.status_code = place_order_result.get("status_code")
-                return place_order_result
+                    rpc.response_len_setter(response_len=1)
+                    rpc.publish(
+                        message={
+                            "cart": {
+                                "action": "delete_cart",
+                                "body": {
+                                    "user_id": auth_header[0].get("user_id")
+                                }
+                            }
+                        },
+                        headers={'cart': True}
+                    )
+                    response.status_code = place_order_result.get("status_code")
+                    return place_order_result
+            else:
+                delete_order_reserving_fail(place_order_result.get("order_object"))
+                check_out = check_price_qty(auth_header, cart, response)
+                response.status_code = 202
+                return {"success": False, "message": check_out.get("message")}
         else:
-            response.status_code = 202
-            return {"success": False, "message": check_out.get("message")}
+
+            response.status_code = place_order_result.get("status_code")
+            return place_order_result
+    else:
+        response.status_code = 202
+        return {"success": False, "message": check_out.get("message")}
