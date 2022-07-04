647f8e8d3023ec8d1396fe29a0eea4caa95fd683
zhra-jm <zahrajam1999@gmail.com>
2022-07-04 11:11:32 +0430

    rabbit removed

--- source/routers/customer/controllers/router_auth.py
+++ source/routers/customer/controllers/router_auth.py
@@ -6,7 +6,6 @@ from fastapi import status, APIRouter
 from source.message_broker.rabbit_server import RabbitRPC
 from source.routers.customer.module.auth import AuthHandler
 from source.routers.customer.validators import validation_auth
-from source.message_broker.rabbitmq import RabbitRPC as NewRabbit
 
 # from customer.modules import log
 
@@ -17,8 +16,6 @@ router_auth = APIRouter(
 )
 
 auth_handler = AuthHandler()
-
-new_rpc = NewRabbit(exchange_name='headers_exchange', timeout=5)
 
 
 # generate and send mobile number validations to front side
