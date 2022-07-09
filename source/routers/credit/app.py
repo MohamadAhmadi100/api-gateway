6c4369b3127f76b415b2ac90656e417340209663
Mohsen Yousefi <mohsen.u3fi@hotmail.com>
2022-07-09 17:00:56 +0430

    credit

---
+++ source/routers/credit/app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, responses
+from starlette.exceptions import HTTPException as starletteHTTPException
+
+from source.config import settings
+from source.routers.customer.module.auth import AuthHandler
+from source.routers.order.controllers.checkout_step import first_step_order
+from source.routers.order.controllers.edit_order import edit_order
+from source.routers.order.controllers.final_step import final_step_order
+from source.routers.order.controllers.get_orders import get_order
+from source.routers.order.controllers.payment_step import payment_step_order
+from source.routers.order.controllers.shipment_steps import shipment_step
+
+TAGS = [
+    {
+        "name": "Order",
+        "description": "ORDER CRUD"
+    }
+]
+
+app = FastAPI(
+    title="Order API",
+    description="This is order gateway MicroService",
+    version="0.1.0",
+    openapi_tags=TAGS,
+    docs_url="/docs/",
+    redoc_url="/redoc/",
+    debug=settings.DEBUG_MODE
+)
+
+auth_handler = AuthHandler()
+
+app.include_router(first_step_order)
+app.include_router(payment_step_order)
+app.include_router(shipment_step)
+app.include_router(final_step_order)
+app.include_router(get_order)
+app.include_router(edit_order)
+
+
+# customize exception handler of fast api
+@app.exception_handler(starletteHTTPException)
+def validation_exception_handler(request, exc):
+    return responses.JSONResponse(exc.detail, status_code=exc.status_code)
---
+++ source/routers/credit/controller/credit_router.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from fastapi import Response, Depends
+
+from source.message_broker.rabbit_server import RabbitRPC
+from source.routers.customer.module.auth import AuthHandler
+
+credit = APIRouter()
+auth_handler = AuthHandler()
+
+
+@credit.get("/create_credit/", tags=["customer_side"])
+def customer_credit(response: Response,
+                    auth_header=Depends(auth_handler.check_current_user_tokens)):
+    user, auth = auth_header
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        rpc.response_len_setter(response_len=1)
+        order_response = rpc.publish(
+            message={
+                "credit": {
+                    "action": "create_credit",
+                    "body": {
+                        "customer_id": user.get("user_id"),
+                    }
+                }
+            },
+            headers={'credit': True}
+        ).get("credit", {})
