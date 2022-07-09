d340224de17f4745d9a598ca0ff35019691938ff
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-07-09 14:10:09 +0430

    fix fav team and table

--- source/routers/mobile_app/controllers/football_controller.py
+++ source/routers/mobile_app/controllers/football_controller.py
@@ -1,10 +1,11 @@
 from typing import Optional
 
-from fastapi import APIRouter, HTTPException, Query, Depends, Body
+from fastapi import APIRouter, HTTPException, Query, Depends, Body, Header
 
 from source.helpers.case_converter import convert_case
 from source.message_broker.rabbit_server import RabbitRPC
 from source.routers.customer.module.auth import AuthHandler
+from source.routers.mobile_app.validators.model import PredictClass
 
 router = APIRouter()
 
@@ -13,17 +14,26 @@ auth_handler = AuthHandler()
 
 @router.get("/get_league_table", tags=["Football"])
 def get_league_table(
+        access: Optional[str] = Header(None),
+        refresh: Optional[str] = Header(None)
 ):
     """
     Get league table
     """
+    user_id = None
+    if access or refresh:
+        user_data, tokens = auth_handler.check_current_user_tokens(access, refresh)
+        user_id = user_data.get("user_id")
+
     with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
         rpc.response_len_setter(response_len=1)
         product_result = rpc.publish(
             message={
                 "mobile_app": {
                     "action": "get_league_table",
-                    "body": {}
+                    "body": {
+                        "user_id": user_id
+                    }
                 }
             },
             headers={'mobile_app': True}
@@ -90,6 +100,7 @@ def get_favorite_teams(auth_header=Depends(auth_handler.check_current_user_token
     """
     Get favorite teams
     """
+    user, token_dict = auth_header
     with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
         rpc.response_len_setter(response_len=1)
         product_result = rpc.publish(
@@ -97,7 +108,7 @@ def get_favorite_teams(auth_header=Depends(auth_handler.check_current_user_token
                 "mobile_app": {
                     "action": "get_favorite_teams",
                     "body": {
-                        "customer_id": auth_header.user_id
+                        "customer_id": 0
                     }
                 }
             },
@@ -190,6 +201,39 @@ def update_favorite_team(
                         "customer_id": user.get("user_id"),
                         "team_name": team_name,
                         "new_team_name": new_team_name
+                    }
+                }
+            },
+            headers={'mobile_app': True}
+        )
+        product_result = product_result.get("mobile_app", {})
+        if product_result.get("success"):
+            return convert_case({"message": product_result.get("message")}, 'camel')
+        raise HTTPException(status_code=product_result.get("status_code", 500),
+                            detail={"error": product_result.get("error", "Something went wrong")})
+
+
+# predicting match result
+@router.post("/predict_match_result/", tags=["PredictMatchResult"])
+def predict_match_result(
+        auth_header=Depends(auth_handler.check_current_user_tokens),
+        item: PredictClass = Body(...)
+):
+    """
+    Predict match result
+    """
+    user, token_dict = auth_header
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        rpc.response_len_setter(response_len=1)
+        product_result = rpc.publish(
+            message={
+                "mobile_app": {
+                    "action": "predict_match_result",
+                    "body": {
+                        "customer_id": user.get("user_id"),
+                        "match_id": match_id,
+                        "team_name": team_name,
+                        "result": result
                     }
                 }
             },
--- source/routers/mobile_app/validators/model.py
+++ source/routers/mobile_app/validators/model.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from pydantic import BaseModel
+from enum import Enum
 
 
 class ForceUpdate(BaseModel):
@@ -16,5 +19,28 @@ class ForceUpdate(BaseModel):
                 "os_type": "android",
                 "force_update": True,
                 "link_download": "https://google.com"
+            }
+        }
+
+
+class MatchResult(str, Enum):
+    Win = "Win"
+    Loss = "loss"
+    Draw = "Draw"
+
+
+class PredictClass(BaseModel):
+    game_id: int
+    match_result: MatchResult
+    home_team_score: Optional[int] = None
+    away_team_score: Optional[int] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "game_id": 1,
+                "match_result": "Draw",
+                "home_team_score": 1,
+                "away_team_score": 1
             }
         }
