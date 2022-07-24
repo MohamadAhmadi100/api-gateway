3659facb076a8cf1c1486d417963b3273523e677
zhra-jm <zahrajam1999@gmail.com>
2022-07-24 16:01:26 +0430

    invoker debug mode fixed

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+
+
+gettrace = getattr(sys, 'gettrace', None)
+
+if gettrace is None:
+    print('No sys.gettrace')
+elif gettrace():
+    print('Hmm, Big Debugger is watching me')
+else:
+    print("Let's do something interesting")
+    print(1 / 0)
