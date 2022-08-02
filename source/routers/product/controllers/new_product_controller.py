2a054705ba513325bbe052adb80b5ce4f5464507
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-08-02 12:41:11 +0430

    added edit product

--- source/routers/product/controllers/new_product_controller.py
+++ source/routers/product/controllers/new_product_controller.py
@@ -8,13 +8,13 @@ from source.message_broker.rabbit_server import RabbitRPC
 from source.routers.product.validators.price_models import UpdatePrice
 from source.routers.product.validators.kowsar import KowsarConfig, KowsarGroup, KowsarPart
 from source.routers.product.validators.price_models import Price
-from source.routers.product.validators.product import Product, AddAttributes
+from source.routers.product.validators.product import Product, AddAttributes, EditProduct
 from source.routers.product.validators.quantity_models import UpdateQuantity, Quantity
 
 router = APIRouter()
 
 
-@router.post("/kowsar/static/")
+@router.post("/kowsar/static/", tags=["Kowsar"])
 def create_kowsar_static_configs(item: KowsarConfig):
     """
     Create a new kowsar group
@@ -42,7 +42,7 @@ def create_kowsar_static_configs(item: KowsarConfig):
 
 
 # get kowsar static configs by config_type
-@router.get("/kowsar/static/{config_type}/")
+@router.get("/kowsar/static/{config_type}/", tags=["Kowsar"])
 def get_kowsar_static_configs_by_config_type(config_type: str):
     """
     Get kowsar static configs by config_type
@@ -67,7 +67,7 @@ def get_kowsar_static_configs_by_config_type(config_type: str):
                             detail={"error": product_result.get("error", "Something went wrong")})
 
 
-@router.post("/kowsar/create_kowsar_group/")
+@router.post("/kowsar/create_kowsar_group/", tags=["Kowsar"])
 def create_kowsar_group(
         response: Response,
         item: KowsarGroup
@@ -99,7 +99,7 @@ def create_kowsar_group(
                             detail={"error": product_result.get("error", "Something went wrong")})
 
 
-@router.post("/kowsar/create_system_code/")
+@router.post("/kowsar/create_system_code/", tags=["Kowsar"])
 def create_system_code(
         response: Response,
         item: KowsarPart
@@ -131,7 +131,7 @@ def create_system_code(
                             detail={"error": product_result.get("error", "Something went wrong")})
 
 
-@router.get("/kowsar/items/{systemCode}/")
+@router.get("/kowsar/items/{systemCode}/", tags=["Kowsar"])
 def get_kowsar_items(
         response: Response,
         system_code: str = Path(..., min_length=2, max_length=16, alias='systemCode')
@@ -160,7 +160,7 @@ def get_kowsar_items(
                             detail={"error": product_result.get("error", "Something went wrong")})
 
 
-@router.post("/product/create_product/")
+@router.post("/product/create_product/", tags=["Product"])
 def create_product(
         response: Response,
         item: Product
@@ -191,7 +191,7 @@ def create_product(
                             detail={"error": product_result.get("error", "Something went wrong")})
 
 
-@router.get("/product/get_product_attributes/{systemCode}/")
+@router.get("/product/get_product_attributes/{systemCode}/", tags=["Product"])
 def get_product_attributes(response: Response,
                            system_code: str = Path(..., min_length=25, max_length=25, alias='systemCode')) -> dict:
     """
@@ -218,7 +218,7 @@ def get_product_attributes(response: Response,
                             detail={"error": product_attrs_result.get("error", "Something went wrong")})
 
 
-@router.post("/product/add_attributes/")
+@router.post("/product/add_attributes/", tags=["Product"])
 def add_attributes(response: Response,
                    item: AddAttributes = Body(..., example={
                        "systemCode": "100104021006",
@@ -269,7 +269,7 @@ def add_attributes(response: Response,
                             detail={"error": product_result.get("error", "Something went wrong")})
 
 
-@router.get("/product/get_backoffice/")
+@router.get("/product/get_backoffice/", tags=["Product"])
 def get_product_backoffice(system_code: str = Query(..., min_length=25, max_length=25, alias='systemCode')) -> dict:
     """
     Get product backoffice
@@ -294,7 +294,7 @@ def get_product_backoffice(system_code: str = Query(..., min_length=25, max_leng
                             detail={"error": product_result.get("error", "Something went wrong")})
 
 
-@router.post("/product/price/")
+@router.post("/product/price/", tags=["Product"])
 def set_product_price(item: Price, response: Response) -> dict:
     """
     set product(12 digits) price according to customer type and warehouse
@@ -325,7 +325,7 @@ def set_product_price(item: Price, response: Response) -> dict:
                             detail={"error": pricing_result.get("error", "Something went wrong")})
 
 
-@router.put("/product/price/")
+@router.put("/product/price/", tags=["Product"])
 def update_product_price(item: UpdatePrice, response: Response) -> dict:
     """
     update product(12 digits) price according to customer type and warehouse
@@ -356,7 +356,7 @@ def update_product_price(item: UpdatePrice, response: Response) -> dict:
                             detail={"error": pricing_result.get("error", "Something went wrong")})
 
 
-@router.get("/product/stock/{systemCode}/")
+@router.get("/product/stock/{systemCode}/", tags=["Product"])
 def get_product_stock(response: Response,
                       system_code: str = Path(..., min_length=25, max_length=25, alias="systemCode")) -> dict:
     """
@@ -384,7 +384,7 @@ def get_product_stock(response: Response,
                                                 action="camel"))
 
 
-@router.post("/product/quantity/")
+@router.post("/product/quantity/", tags=["Product"])
 def set_product_quantity(item: Quantity, response: Response) -> dict:
     """
     set product(12 digits) quantity according to customer type and warehouse
@@ -412,7 +412,7 @@ def set_product_quantity(item: Quantity, response: Response) -> dict:
                             detail={"error": quantity_result.get("error", "Something went wrong")})
 
 
-@router.put("/product/quantity/")
+@router.put("/product/quantity/", tags=["Product"])
 def update_product_quantity(item: UpdateQuantity, response: Response) -> dict:
     with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
         rpc.response_len_setter(response_len=1)
@@ -433,7 +433,38 @@ def update_product_quantity(item: UpdateQuantity, response: Response) -> dict:
                             detail={"error": quantity_result.get("error", "Something went wrong")})
 
 
-@router.get("/product/get_product_list_back_office/")
+@router.post("/product/edit_product/{systemCode}", tags=["Product"])
+def edit_product(
+        response: Response,
+        system_code: str = Path(..., min_length=25, max_length=25, alias='systemCode'),
+        item: EditProduct = Body(...)
+) -> dict:
+    """
+    Edit a product by name in main collection in database.
+    """
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        rpc.response_len_setter(response_len=1)
+        product_result = rpc.publish(
+            message={
+                "product": {
+                    "action": "edit_product",
+                    "body": {
+                        "system_code": system_code,
+                        "item": dict(item)
+                    }
+                }
+            },
+            headers={'product': True}
+        )
+        product_result = product_result.get("product", {})
+        if product_result.get("success"):
+            response.status_code = product_result.get("status_code", 200)
+            return convert_case(product_result.get("message"), 'camel')
+        raise HTTPException(status_code=product_result.get("status_code", 500),
+                            detail={"error": product_result.get("error", "Something went wrong")})
+
+
+@router.get("/product/get_product_list_back_office/", tags=["Product"])
 def get_product_list_back_office(
         response: Response,
         brands: Optional[List[str]] = Query(None),
@@ -460,50 +491,6 @@ def get_product_list_back_office(
     """
     with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
         rpc.response_len_setter(response_len=1)
-
-        # system_codes_list = []
-        #
-        # quantity_result_filter = rpc.publish(
-        #     message={
-        #         "quantity": {
-        #             "action": "quantity_filter",
-        #             "body": {
-        #                 "quantity_range": [quantity_from, quantity_to] if quantity_from or quantity_to else None,
-        #                 "warehouses": warehouses,
-        #             }
-        #         }
-        #     },
-        #     headers={'quantity': True}
-        # )
-        #
-        # if (quantity_from or quantity_to) and not quantity_result_filter.get("quantity", {}).get("message", {}).get(
-        #         'system_codes', None):
-        #     raise HTTPException(status_code=404, detail={"error": "No products found"})
-        #
-        # if quantity_from or quantity_to:
-        #     system_codes_list = quantity_result_filter.get("quantity", {}).get("message", {}).get('system_codes', [])
-        #
-        # if price_from or price_to:
-        #     system_codes = []
-        #     if quantity_result_filter.get("quantity", {}).get("message", {}).get('system_codes', None):
-        #         system_codes = quantity_result_filter.get("quantity", {}).get("message", {}).get('system_codes', [])
-        #     price_result = rpc.publish(
-        #         message={
-        #             "pricing": {
-        #                 "action": "pricing_filter",
-        #                 "body": {
-        #                     "pricing_range": [price_from, price_to] if price_from or price_to else None,
-        #                     "system_codes": system_codes
-        #                 }
-        #             }
-        #         },
-        #         headers={'pricing': True}
-        #     )
-        #     if not price_result.get("pricing", {}).get("success", {}):
-        #         raise HTTPException(status_code=404, detail={"error": "No products found"})
-        #
-        #     system_codes_list = price_result.get("pricing", {}).get("message", {})
-
         product_result = rpc.publish(
             message={
                 "product": {
@@ -516,83 +503,6 @@ def get_product_list_back_office(
         )
         product_result = product_result.get("product", {})
         message_product = product_result.get("message", {})
-
-        # product_list = list()
-        # for product in message_product['products']:
-        #     rpc.response_len_setter(response_len=2)
-        #     rpc_result = rpc.publish(
-        #         message={
-        #             "pricing": {
-        #                 "action": "get_price",
-        #                 "body": {
-        #                     "system_code": product.get("system_code")
-        #                 }
-        #             },
-        #             "quantity": {
-        #                 "action": "get_quantity",
-        #                 "body": {
-        #                     "system_code": product.get("system_code")
-        #                 }
-        #             }
-        #         },
-        #         headers={'pricing': True, "quantity": True}
-        #     )
-        #
-        #     pricing_result = rpc_result.get("pricing", {})
-        #     quantity_result = rpc_result.get("quantity", {})
-        #     # price_tuples = list()
-        #     if product.get('products'):
-        #         for config in product['products']:
-        #             if pricing_result.get("success"):
-        #                 system_code = config.get("system_code")
-        #                 rpc.response_len_setter(response_len=1)
-        #                 get_stock_result = rpc.publish(
-        #                     message={
-        #                         "quantity": {
-        #                             "action": "get_stock",
-        #                             "body": {
-        #                                 "system_code": system_code
-        #                             }
-        #                         }
-        #                     },
-        #                     headers={'quantity': True}
-        #                 )
-        #                 get_stock_result = get_stock_result.get("quantity", {}).get("message", {})
-        #                 for customer_type, customer_type_obj in pricing_result.get("message", {}).get(
-        #                         "products", {}).get(system_code, {}).get("customer_type", {}).items():
-        #                     for storage, storage_obj in customer_type_obj.get("storages", {}).items():
-        #                         pricing_result['message']["products"][system_code]["customer_type"][customer_type][
-        #                             'storages'][storage]['warehouse_label'] = quantity_result.get("message", {}).get(
-        #                             "products",
-        #                             {}).get(
-        #                             system_code, {}).get("customer_types", {}).get(customer_type, {}).get("storages",
-        #                                                                                                   {}).get(
-        #                             storage, {}).get("warehouse_label", None)
-        #
-        #                         stock_msm = [stock.get("stock") for stock in
-        #                                      get_stock_result.get("storages", []) if
-        #                                      stock.get("storage_id") == storage]
-        #
-        #                         if quantity_result.get("success"):
-        #                             quantity_result.setdefault("message", {}).setdefault('products', {}).setdefault(
-        #                                 system_code, {}).setdefault("customer_types", {}).setdefault(customer_type,
-        #                                                                                              {}).setdefault(
-        #                                 'storages', {}).setdefault(storage, {})['stock'] = stock_msm[
-        #                                 0] if stock_msm else 0
-        #
-        #                 config['price'] = pricing_result.get("message", {}).get("products", {}).get(system_code,
-        #                                                                                             {})
-        #             if quantity_result.get("success"):
-        #                 system_code = config.get("system_code")
-        #                 config['quantity'] = quantity_result.get("message", {}).get("products", {}).get(system_code,
-        #                                                                                                 {})
-        #
-        #     product_system_code = product.get("system_code")
-        #     product['system_code'] = product_system_code[:9] + "-" + product_system_code[9:]
-        #     product_list.append(product)
-        # message_product['products'] = product_list
-        # message_product['filters'][3]['options'] = quantity_result_filter.get("quantity", {}).get("message", {}).get(
-        #     'storages', [])
         if product_result.get("success"):
             response.status_code = product_result.get("status_code", 200)
             return convert_case(message_product, 'camel')
