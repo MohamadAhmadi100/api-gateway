ef9906bf171251e0bf911dfb0f796170d209241a
Mohsen Yousefi <mohsen.u3fi@hotmail.com>
2022-08-10 13:22:31 +0430

    change customer profile

--- source/routers/customer/helpers/profile_view.py
+++ source/routers/customer/helpers/profile_view.py
@@ -11,65 +11,15 @@ def get_profile_info(user_data: dict) -> dict or Exception:
     :param user_data: example: {'user_id': int, 'customer_type': list, 'phone_number': str'}
     :return: dict for response to client or HttpException for 422, 404 or 418 res
     """
-    # with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
-    #     rpc.response_len_setter(response_len=1)
-    #     result = rpc.publish(
-    #         message={
-    #             "customer": {
-    #                 "action": "get_profile",
-    #                 "body": {
-    #                     "customer_phone_number": user_data,
-    #                 }
-    #             }
-    #         },
-    #         headers={'customer': True}
-    #     )
-    # customer_result = result.get("customer", {})
+
     customer_data = new_rpc.publish(
-        message=[profile_funcs.get_profile(customer_phone_number=user_data)]
+        message=[profile_funcs.get_raw_profile(customer_phone_number=user_data.get("phone_number"))]
     )
     if not customer_data.get("success"):
         raise HTTPException(
             status_code=customer_data.get("status_code", 500),
             detail={"error": customer_data.get("error", "Something went wrong")}
         )
-
-    # with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
-    #     rpc.response_len_setter(response_len=1)
-    #     result = rpc.publish(
-    #         message={
-    #             "attribute": {
-    #                 "action": "get_all_attributes_by_assignee",
-    #                 "body": {
-    #                     "name": "customer"
-    #                 }
-    #             }
-    #         },
-    #         headers={'attribute': True}
-    #     )
-    #     attribute_result = result.get("attribute", {})
-    # if not attribute_result.get("success"):
-    #     raise HTTPException(status_code=attribute_result.get("status_code", 500),
-    #                         detail={"error": attribute_result.get("error", "Something went wrong")})
-    # customer_data = customer_result.get("message", {})
-    # attributes = attribute_result.get("message", [])
-    # for attr in attributes:
-    #     if customer_data.get(attr.get("name")) is None or not None:
-    #         attr["value"] = customer_data.get(attr.get("name"))
-    #         customer_data[attr['name']] = attr['value']
-    # with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
-    #     rpc.response_len_setter(response_len=1)
-    #     address_response = rpc.publish(
-    #         message={
-    #             "address": {
-    #                 "action": "get_customer_addresses",
-    #                 "body": {
-    #                     "customerId": str(user_data.get("user_id"))
-    #                 }
-    #             }
-    #         },
-    #         headers={'address': True}
-    #     ).get("address", {})
     with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
         rpc.response_len_setter(response_len=1)
         address_response = rpc.publish(
@@ -83,12 +33,12 @@ def get_profile_info(user_data: dict) -> dict or Exception:
             },
             headers={'address': True}
         ).get("address", {})
-
+        customer = customer_data.get("message")
         if not address_response.get("success"):
             return customer_data
-        customer_data['customerAddress'] = address_response['result'].get("fullAddress")
-        customer_data['customerCity'] = address_response['result'].get('cityName')
-        customer_data['customerProvince'] = address_response['result'].get('stateName')
-        customer_data['cityID'] = address_response['result'].get('cityId')
-        customer_data['customerProvinceCode'] = address_response['result'].get('stateId')
-    return customer_data
+        customer['customerAddress'] = address_response['result'].get("fullAddress")
+        customer['customerCity'] = address_response['result'].get('cityName')
+        customer['customerProvince'] = address_response['result'].get('stateName')
+        customer['cityID'] = address_response['result'].get('cityId')
+        customer['customerProvinceCode'] = address_response['result'].get('stateId')
+    return customer
