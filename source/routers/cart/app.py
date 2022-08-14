2a777ef7c1e88a60eb3f6235d690bccaff9cadd7
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-08-14 15:32:28 +0430

    customer confirm label

--- source/routers/cart/app.py
+++ source/routers/cart/app.py
@@ -80,7 +80,7 @@ def add_and_edit_product(item: AddCart, response: Response, auth_header=Depends(
             raise HTTPException(status_code=product_result.get("status_code", 500),
                                 detail={"error": product_result.get("error", "Something went wrong")})
         elif not customer_result.get("message", {}).get('customerIsActive'):
-            raise HTTPException(status_code=403, detail={"error": "Customer is not confirmed"})
+            raise HTTPException(status_code=403, detail={"error": "حساب کاربری شما تایید نشده است"})
         else:
 
             ordered_count = [i.get("count") for i in order_result.get('customer_detail') if
