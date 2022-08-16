42828d1e300b6a9790d38640eb39c9fa3615ad41
Zahra Jamshidi <z.jamshidi@aasood.com>
2022-08-16 09:44:43 +0000

    change bank url for n+2 time

--- source/routers/payment/controllers/bank_controller.py
+++ source/routers/payment/controllers/bank_controller.py
@@ -165,7 +165,7 @@ async def set_callback(request: Request, response: Response):
             elif service_name == "order":
                 result = 1 if service_data.get("result") else 2
             return RedirectResponse(
-                f"https://m.aasood.com/payment-result/{result}/{service_data.get('service_id')}")
+                f"https://aasood.com/payment-result/{result}/{service_data.get('service_id')}")
 
 
 @router.post("/closed_tabs", include_in_schema=False)
