7c3b0a113839e05b3529f2a1d2d1c816bf43a29e
SalarRezaee <salar_rezaei@aol.com>
2022-10-25 15:49:55 +0330

    credit get requests api

--- source/routers/credit/validators/credit_validator.py
+++ source/routers/credit/validators/credit_validator.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException
 from pydantic import BaseModel, Field, validator
-from typing import Literal
+from typing import Optional
 
 class AddCredit(BaseModel):
     amount: int = Field(..., alias="amount")
@@ -14,3 +14,14 @@ class AddCredit(BaseModel):
 
 class AcceptCredit(BaseModel):
     referral_number: str = Field(..., alias="referralNumber")
+
+
+
+class RequestsDetail(BaseModel):
+    referral_number: Optional[str] = Field(..., alias="referralNumber")
+    date_from: Optional[str]  = Field(..., alias="dateFrom")
+    date_to: Optional[str]  = Field(..., alias="dateTo")
+    page: int = Field(..., alias="page")
+    per_page: int = Field(..., alias="perPage")
+    accepted: Optional[bool]  = Field(..., alias="accepted")
+    search_box: Optional[str]  = Field(..., alias="searchBox")
