b2ef80b2f404f9488776710d7642518cf4452aa8
m.yousefi <mohsen.u3fi@hotmail.com>
2022-11-08 15:29:52 +0330

    remove device type

--- source/routers/order/controllers/final_step.py
+++ source/routers/order/controllers/final_step.py
@@ -11,7 +11,6 @@ from source.routers.order.helpers.final_helper import place_order, reserve_order
     delete_order_reserving_fail, add_final_flag_to_cart
 from source.routers.order.helpers.payment_helper import wallet_final_consume
 from source.routers.order.helpers.shipment_helper import check_shipment_per_stock
-from source.routers.order.validators.order import final
 from source.routers.payment.controllers.bank_controller import get_url
 from source.routers.payment.validators.payment import SendData
 
