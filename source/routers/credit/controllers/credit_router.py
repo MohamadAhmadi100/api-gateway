eb7d047e00fd89427bfd77b707d64a0119a677fe
s.rezaei <salar_rezaei@aol.com>
2022-11-08 12:13:52 +0330

    post and get api with filter and search for dealership accounting records in credit service

--- source/routers/credit/controllers/credit_router.py
+++ source/routers/credit/controllers/credit_router.py
@@ -3,7 +3,7 @@ from starlette.exceptions import HTTPException
 from fastapi import APIRouter, Response, Depends, Query
 from typing import Union
 from source.message_broker.rabbit_server import RabbitRPC
-from source.routers.credit.validators.credit_validator import AddCredit, AcceptCredit, RequestsDetail
+from source.routers.credit.validators.credit_validator import AddCredit, AcceptCredit, RequestsDetail, AccountingRecords
 from source.routers.customer.helpers.profile_view import get_profile_info
 from source.routers.customer.module.auth import AuthHandler
 
@@ -194,3 +194,48 @@ def accept_dealership_credit(response: Response, data: AcceptCredit,
         ).get("credit", {})
         response.status_code = 200
         return order_response
+
+
+
+############################# Accounting ###########################################
+
+
+
+@credit.post("/get_accounting_records", tags=["Accounting"])
+def get_accounting_records(response: Response, data: AccountingRecords,
+                        # auth_header=Depends(auth_handler.check_current_user_tokens)
+                           ):
+    # user, auth = auth_header
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        rpc.response_len_setter(response_len=1)
+        # customer = get_profile_info(auth_header[0])
+        requests_details = data.dict()
+        credit_response = rpc.publish(
+            message={
+                "credit": {
+                    "action": "get_accounting_records",
+                    "body": {
+                        "dealership_name": requests_details.get("dealership_name"),
+                        "order_number": requests_details.get("order_number"),
+                        "date_from": requests_details.get("date_from"),
+                        "date_to": requests_details.get("date_to"),
+                        "page": requests_details.get("page"),
+                        "per_page": requests_details.get("per_page"),
+                        "payment_status": requests_details.get("payment_status"),
+                        "unit_price_from": requests_details.get("unit_price_from"),
+                        "unit_price_to": requests_details.get("unit_price_to"),
+                        "total_price_from": requests_details.get("total_price_from"),
+                        "total_price_to": requests_details.get("total_price_to"),
+                        "count_from": requests_details.get("unit_price_from"),
+                        "count_to": requests_details.get("count_to"),
+                        "wage_from": requests_details.get("wage_from"),
+                        "wage_to": requests_details.get("wage_to")
+                    }
+                }
+            },
+            headers={'credit': True}
+        ).get("credit", {})
+        if credit_response.get("success"):
+            response.status_code = 200
+            return credit_response
+        return credit_response
