a003af4edd5b461f8ec905cd22b38a3dd5c9b2dd
s.rezaei <salar_rezaei@aol.com>
2022-11-20 17:34:08 +0330

    neighborhood_id is added to shipment

--- source/routers/shipment/validators/shipment.py
+++ source/routers/shipment/validators/shipment.py
@@ -8,7 +8,7 @@ class Stock(BaseModel):
     stockId: str
     origin: str
     destination: str
-    # nbId: str | None = None
+    nbId: Optional[str]
     weight: int
     totalPrice: int
     totalItem: int
