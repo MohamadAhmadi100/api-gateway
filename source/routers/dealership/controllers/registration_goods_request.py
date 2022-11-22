bd2882bf9c80a82fcd1d488a3a9f35c2a70de915
s.rezaei <salar_rezaei@aol.com>
2022-11-22 17:28:24 +0330

    change customer type in dealership

--- source/routers/dealership/controllers/registration_goods_request.py
+++ source/routers/dealership/controllers/registration_goods_request.py
@@ -75,7 +75,7 @@ def create_request(data: RequestGoods,
                                     "body": {
                                         "referral_number": referral_response.get("message"),
                                         "customer_id": user.get("user_id"),
-                                        "customer_type": user.get("customer_type")[0],
+                                        "customer_type": user.get("customer_type"),
                                         "data": data.dict(),
                                     }
                                 }
