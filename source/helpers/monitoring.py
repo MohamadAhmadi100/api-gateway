a3a7fe035ce15dab73a780a7ea730a4f99336b62
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-12-19 17:12:17 +0330

    added custom metrics

---
+++ source/helpers/monitoring.py
@@ -0,0 +1,33 @@
+import time
+
+from starlette.middleware.base import BaseHTTPMiddleware
+
+RESPONSE_TIME = {}
+
+
+class Monitoring(BaseHTTPMiddleware):
+    def __init__(self, app):
+        super().__init__(app)
+
+    async def dispatch(self, request, call_next):
+        start = round(time.time() * 1000)
+        response = await call_next(request)
+        end = round(time.time() * 1000)
+        RESPONSE_TIME[request.url.path] = {
+            "method": request.method,
+            "value": end - start
+        }
+        return response
+
+    @staticmethod
+    def metric_formatter(metric_name, method, path, value):
+        return f'{metric_name}{{method="{method}", path="{path}"}} {value}'
+
+    @staticmethod
+    def metrics(path):
+        response = ""
+        for k, v in RESPONSE_TIME.items():
+            if k.startswith("/" + path):
+                response += Monitoring.metric_formatter("response_time", v.get("method"), k, v.get("value")) + "\n"
+
+        return response
