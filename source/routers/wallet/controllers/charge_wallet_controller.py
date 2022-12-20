703d79aac89155f5aa6f2c6c8255fdbf3ac8a7ba
root <moafi@aasood.com>
2022-12-20 16:29:54 +0330

    add customer type to charge wallet

--- source/routers/wallet/controllers/charge_wallet_controller.py
+++ source/routers/wallet/controllers/charge_wallet_controller.py
@@ -3,16 +3,20 @@ from source.message_broker.rabbit_server import RabbitRPC
 from source.routers.payment.controllers.bank_controller import get_url
 from source.routers.payment.validators.payment import SendData
 from source.routers.wallet.validators.charge_wallet import Charge
-from fastapi import Response, APIRouter
+from fastapi import Response, APIRouter, Depends
+from source.routers.customer.module.auth import AuthHandler
 
 router = APIRouter()
+auth = AuthHandler()
 
 
 @router.put("/charge_wallet", tags=["charge wallet amount"])
 def charge_wallet(
         charge_data: Charge,
         response: Response,
+        auth_header=Depends(auth.check_current_user_tokens)
 ):
+    sub_data, token_data = auth_header
     with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
         rpc.response_len_setter(response_len=1)
         wallet_response = rpc.publish(
@@ -21,6 +25,7 @@ def charge_wallet(
                     "action": "create_transaction",
                     "body": {
                         "data": {
+                            "customer_type": sub_data.get("customer_type"),
                             "device_type": charge_data.device_type,
                             "customer_id": charge_data.customer_id,
                             "amount": charge_data.amount,
@@ -39,7 +44,7 @@ def charge_wallet(
         if wallet_response.get("success"):
             transaction = wallet_response.get("message")
             send_data = SendData(
-                amount=int(charge_data.amount)*10,
+                amount=int(charge_data.amount) * 10,
                 customerId=int(charge_data.customer_id),
                 serviceName="wallet",
                 serviceId=f'w{transaction.get("transactionId")}',
@@ -63,5 +68,3 @@ def charge_wallet(
                 headers={'wallet': True}
             )
             return payment_result
-
-
