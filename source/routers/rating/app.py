066fdfd4ef43f31352e9a2763a917d42fef10a8e
root <moafi@aasood.com>
2023-01-14 12:27:26 +0330

    add rating rout

--- source/routers/rating/app.py
+++ source/routers/rating/app.py
@@ -1,8 +1,9 @@
 from source.message_broker.rabbit_server import RabbitRPC
 from source.config import settings
+from typing import Optional
 from source.routers.customer.module.auth import AuthHandler
-from source.routers.rating.validator.rate_wallidator import InsertRate
-from fastapi import HTTPException, Response, Depends, APIRouter, FastAPI
+from source.routers.rating.validator.rate_wallidator import InsertRate, LikesParameters
+from fastapi import HTTPException, Response, Depends, APIRouter, FastAPI, Query
 
 TAGS = [
     {
@@ -24,6 +25,74 @@ router = APIRouter()
 auth = AuthHandler()
 
 
+@app.get("/get-property", tags=["customer side"])
+def get_property(response: Response,
+                 # auth_header=Depends(auth.check_current_user_tokens)
+                 ):
+    # sub_data, token_data = auth_header
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        # response.headers["accessToken"] = token_data.get("access_token")
+        # response.headers["refreshToken"] = token_data.get("refresh_token")
+        rpc.response_len_setter(response_len=1)
+        rating_response = rpc.publish(
+            message={
+                "rating": {
+                    "action": "get_all_property",
+                    "body": {}
+                }
+
+            },
+            headers={'rating': True}
+        ).get("rating", {})
+
+        if rating_response.get("success"):
+            response.status_code = rating_response.get("status_code", 200)
+            return rating_response
+        elif not rating_response.get("success"):
+            response.status_code = rating_response.get("status_code", 417)
+            return rating_response
+        raise HTTPException(status_code=rating_response.get("status_code", 500),
+                            detail={"error": rating_response.get("error", "Wallet service Internal error")})
+
+
+@app.put("/edit-likes", tags=["customer side"])
+def edit_likes(
+        response: Response,
+        rate_id: int = Query(..., alias="rateId"),
+        parameter: Optional[LikesParameters] = Query(..., alias="parameter"),
+
+        # auth_header=Depends(auth.check_current_user_tokens)
+):
+    # sub_data, token_data = auth_header
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        # response.headers["accessToken"] = token_data.get("access_token")
+        # response.headers["refreshToken"] = token_data.get("refresh_token")
+        rpc.response_len_setter(response_len=1)
+
+        rating_response = rpc.publish(
+            message={
+                "rating": {
+                    "action": "update_likes",
+                    "body": {
+
+                        "rate_id": rate_id,
+                        "parameter": parameter
+                    }
+                }
+            },
+            headers={'rating': True}
+        ).get("rating", {})
+
+        if rating_response.get("success"):
+            response.status_code = rating_response.get("status_code", 200)
+            return rating_response
+        elif not rating_response.get("success"):
+            response.status_code = rating_response.get("status_code", 417)
+            return rating_response
+        raise HTTPException(status_code=rating_response.get("status_code", 500),
+                            detail={"error": rating_response.get("error", "Wallet service Internal error")})
+
+
 @app.post("/insert-rate", tags=["customer side"])
 def insert_rate(data: InsertRate, response: Response,
                 # auth_header=Depends(auth.check_current_user_tokens)
@@ -33,20 +102,52 @@ def insert_rate(data: InsertRate, response: Response,
         # response.headers["accessToken"] = token_data.get("access_token")
         # response.headers["refreshToken"] = token_data.get("refresh_token")
         rpc.response_len_setter(response_len=1)
+        data = data.get_insert_rate()
         rating_response = rpc.publish(
             message={
                 "rating": {
                     "action": "create_new_rate",
                     "body": {
-                        "system_code": data.system_code,
-                        "sku": data.sku,
-                        "customer_id": data.customer_id,
-                        "customer_name": data.customer_name,
-                        "customer_type": data.customer_type,
-                        "seller_id": data.seller_id,
-                        "seller_name": data.seller_name,
-                        "details": data.details,
-                        "properties": data.properties,
+                        "system_code": data.get("system_code"),
+                        "sku": data.get("sku"),
+                        "customer_id": data.get("customer_id"),
+                        "customer_name": data.get("customer_name"),
+                        "customer_type": data.get("customer_type"),
+                        "seller_id": data.get("seller_id"),
+                        "seller_name": data.get("seller_name"),
+                        "details": data.get("details", {}),
+                    }
+                }
+            },
+            headers={'rating': True}
+        ).get("rating", {})
+
+        if rating_response.get("success"):
+            response.status_code = rating_response.get("status_code", 200)
+            return rating_response
+        elif not rating_response.get("success"):
+            response.status_code = rating_response.get("status_code", 417)
+            return rating_response
+        raise HTTPException(status_code=rating_response.get("status_code", 500),
+                            detail={"error": rating_response.get("error", "Wallet service Internal error")})
+
+
+@app.get("/product-rate", tags=["customer side"])
+def get_products_rates(response: Response,
+                       system_code: str = Query(..., alias="systemCode"),
+                       page: int = Query(..., alias="page"),
+                       per_page: int = Query(..., alias="perPage")
+                       ):
+    with RabbitRPC(exchange_name='headers_exchange', timeout=5) as rpc:
+        rpc.response_len_setter(response_len=1)
+        rating_response = rpc.publish(
+            message={
+                "rating": {
+                    "action": "get_products_rates",
+                    "body": {
+                        "system_code": system_code,
+                        "page": page,
+                        "per_page": per_page,
                     }
                 }
             },
--- source/routers/rating/validator/rate_wallidator.py
+++ source/routers/rating/validator/rate_wallidator.py
@@ -8,31 +8,68 @@ class SortType(str, Enum):
     descend = "descend"
 
 
+class LikesParameters(str, Enum):
+    likes = "likes"
+    dislikes = "dislikes"
+
+
 class SubDetails(BaseModel):
     negative: List[str] = Field(None, alias="negative")
     positive: List[str] = Field(None, alias="positive")
     description: str = Field(None, alias="description")
 
+    def get_sub_details(self):
+        return {
+            "negative": list(self.negative),
+            "positive": list(self.positive),
+            "description": str(self.description),
+        }
 
-class Properties(BaseModel):
-    product_property: List[dict] = Field(None, alias="productProperty")
-    seller_property: List[dict] = Field(None, alias="sellerProperty")
+
+
+class DetailsType(BaseModel):
+    comments: Optional[SubDetails] = Field(None, alias="comments")
+    rate: dict = Field(None, alias="rate")
+    average: float = Field(..., alias="average")
+
+    def get_details_type(self):
+        return {
+            "comments": dict(self.comments.get_sub_details()),
+            "rate": dict(self.rate),
+            "average": float(self.average),
+        }
 
 
 class Details(BaseModel):
-    product_details: Optional[SubDetails] = Field(None, alias="productDetails")
-    seller_details: Optional[SubDetails] = Field(None, alias="sellerDetails")
+    product: Optional[DetailsType] = Field(..., alias="product")
+    seller: Optional[DetailsType] = Field(..., alias="seller")
+
+    def get_details(self):
+        return {
+            "product": dict(self.product.get_details_type()),
+            "seller": dict(self.seller.get_details_type())
+        }
 
 
 class InsertRate(BaseModel):
-    system_code: str = Field(None, alias="systemCode")
-    sku: str = Field(None, alias="sku")
+
+    system_code: str = Field(..., alias="systemCode")
+    sku: str = Field(..., alias="sku")
     customer_id: int = Field(..., alias="customerId")
     customer_name: str = Field(..., alias="customerName")
     customer_type: str = Field(..., alias="customerType")
-    seller_id: int = Field(None, alias="sellerId")
-    seller_name: int = Field(None, alias="sellerName")
-    details: Optional[Details] = Field(None, alias="details")
-    properties: Optional[Properties] = Field(None, alias="properties")
+    seller_id: int = Field(..., alias="sellerId")
+    seller_name: str = Field(..., alias="sellerName")
+    details: Optional[Details] = Field(..., alias="details")
 
-
+    def get_insert_rate(self):
+        return {
+            "system_code": self.system_code,
+            "sku": self.sku,
+            "customer_id": self.customer_id,
+            "customer_name": self.customer_name,
+            "customer_type": self.customer_type,
+            "seller_id": self.seller_id,
+            "seller_name": self.seller_name,
+            "details": self.details.get_details(),
+        }
