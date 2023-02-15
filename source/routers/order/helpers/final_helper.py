d07db2bf3b69a443e0aab73346546596069219aa
m.yousefi <mohsen.u3fi@hotmail.com>
2023-02-15 16:06:17 +0330

    add use copon to order fix bugs

--- source/routers/order/helpers/final_helper.py
+++ source/routers/order/helpers/final_helper.py
@@ -52,21 +52,22 @@ def handle_order_bank_callback(result, response):
 
             if result['is_paid']:
                 # coupon
-                rpc.response_len_setter(response_len=1)
-                rpc.publish(
-                    message={
-                        "coupon": {
-                            "action": "use_coupon",
-                            "body": {
-                                "coupon_id": order_get_response.get("order_object")['coupon'].get('couponId'),
-                                "customer_id": order_get_response.get("order_object")['customer']['id'],
-                                "token": order_get_response.get("order_object")['coupon'].get('token'),
-                                "order_number": order_get_response.get("order_object")['orderNumber']
+                if order_get_response.get("order_object").get('coupon') is not None:
+                    rpc.response_len_setter(response_len=1)
+                    rpc.publish(
+                        message={
+                            "coupon": {
+                                "action": "use_coupon",
+                                "body": {
+                                    "coupon_id": order_get_response.get("order_object")['coupon'].get('couponId'),
+                                    "customer_id": order_get_response.get("order_object")['customer']['id'],
+                                    "token": order_get_response.get("order_object")['coupon'].get('token'),
+                                    "order_number": order_get_response.get("order_object")['orderNumber']
+                                }
                             }
-                        }
-                    },
-                    headers={"coupon": True}
-                ).get("coupon")
+                        },
+                        headers={"coupon": True}
+                    ).get("coupon")
                 # send sms
                 rpc.response_len_setter(response_len=1)
                 rpc.publish(
